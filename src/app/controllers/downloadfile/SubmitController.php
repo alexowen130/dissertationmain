3902ce7c27b83ef96f45e741111775c65d9128e4
Alex <owen.alexanderpj@gmail.com>
2017-08-10 15:59:58 +0100

    Updated File upload proceedure

--- src/app/controllers/downloadfile/SubmitController.php
+++ src/app/controllers/downloadfile/SubmitController.php
@@ -7,10 +7,12 @@ use App\models\download;
 use Slim\Http\Request;
 use Slim\Http\Response;
 use Slim\Http\UploadedFile;
+use App\lib\moveUploadedFile;
 
 class SubmitController extends BaseController
 {
 
+	//Renders template that is needed
 	public function getDownload($request, $response)
 	{
 	 	return $this->container->view->render($response, 'Filedownload/filedownload.html');
@@ -20,25 +22,61 @@ class SubmitController extends BaseController
 	public function postDownload($request, $response)
 	{
 
+		//Gets the file from template
+		$uploadedFile = $request->getUploadedFiles();
 
-		$uploadedFile = $request->getUploadedFiles()['file'];
+		$uploadedFile = $uploadedFile['file'];
 
+
+		//Checks if any Errors are present
    		if ($uploadedFile->getError() === UPLOAD_ERR_OK) {
 
 	
+			//Gets FileName as stored on Server
    			$uploadedFileName = $request->getUploadedFiles()['file']->getClientFilename();
+
+   			//Gets the FileType
    			$mediaType = $request->getUploadedFiles()['file']->getClientMediaType();
 
+   			//Moves file to directory
    			$filename = moveUploadedFile('/var/www/downloads', $uploadedFile);
 
-   			var_dump($uploadedFileName);
 
-			move_uploaded_file($uploadedFileName, '/var/www/downloads');   			
+   			//Adds File to DB
+			$downloads = Download::create([
 
+			'filename' => $uploadedFileName,
+			'filetype' => $mediaType,
+			'filelocation' => $filename
 
-		$this->container->flash->addMessage('info', 'You have sucessfully Uploaded a File! Are there any more to submit');
-		
+			]);
+
+   			//Displays message confirming Sucess
+			$this->container->flash->addMessage('info', 'You have sucessfully Uploaded a File! Are there any more to submit');
+
+			//Returns to Submission Page
+			return $response->withRedirect($this->container->router->pathFor('submit.download'));
 	}
 
+	//Displays Errors
+	$this->container->flash->addMessage('error', 'We are unable to upload your file please try again!');
+
+	//Returns to Submission Page
+	return $response->withRedirect($this->container->router->pathFor('submit.download'));
+
 }
+
+}
+
+//Function from Slim Documentation that allows the file to be moved into a specificed directory
+
+function moveUploadedFile($directory, UploadedFile $uploadedFile)
+{
+    $extension = pathinfo($uploadedFile->getClientFilename(), PATHINFO_EXTENSION);
+    $basename = bin2hex(random_bytes(8)); // see http://php.net/manual/en/function.random-bytes.php
+    $filename = sprintf('%s.%0.8s', $basename, $extension);
+
+    $uploadedFile->moveTo($directory . DIRECTORY_SEPARATOR . $filename);
+
+    return $filename;
 }
